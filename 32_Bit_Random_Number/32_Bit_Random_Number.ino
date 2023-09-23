27d577a7ea5f34b5aa61404e9c528f2f14c83a45
ScottBillingsley <61607063+ScottBillingsley@users.noreply.github.com>
2023-09-23 11:26:12 -0500

    Update 32_Bit_Random_Number.ino

--- 32_Bit_Random_Number/32_Bit_Random_Number.ino
+++ 32_Bit_Random_Number/32_Bit_Random_Number.ino
@@ -135,8 +135,8 @@ volatile uint8_t random_number = 0;
 
 /**************************  Functions ****************************/
 /* Get a random number between from and to */
-uint8_t get_random(uint8_t floor, uint8_t celing){
-  float coef = (1.0 / ((float)255 / (float)(celing - floor)));
+uint8_t get_random(uint8_t floor, uint8_t ceiling){
+  float coef = (1.0 / ((float)255 / (float)(ceiling - floor)));
   uint8_t r = (floor + (byte)((float)random_number * coef));  
   return r;
 }
